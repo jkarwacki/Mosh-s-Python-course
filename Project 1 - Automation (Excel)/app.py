55a49e7ea9da60d74800f4d6bc9fcc852a3c6fb8
Julian <karwacki@kth.se>
2020-03-20 13:58:18 +0100

    :tada: latest commit

---
+++ Project 1 - Automation (Excel)/app.py
@@ -0,0 +1,27 @@
+import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
+
+
+def process_workbook(filename):
+    # function that takes in a spreadsheet and manipulates it, in this case multiplying all values on column 3 w/ 0.9
+    # and adding the resulting values in column 4, then saving the file
+    wb = xl.load_workbook(filename)
+    sheet = wb['Sheet1']
+
+    for row in range(2, sheet.max_row + 1):
+        cell = sheet.cell(row, 3)
+        corrected_price = cell.value * 0.9
+        corrected_price_cell = sheet.cell(row, 4)
+        corrected_price_cell.value = corrected_price
+
+    values = Reference(sheet,
+                       min_row=2,
+                       max_row=sheet.max_row,
+                       min_col=4,
+                       max_col=4)
+
+    chart = BarChart()
+    chart.add_data(values)
+    sheet.add_chart(chart, 'e2')
+
+    wb.save(filename)
